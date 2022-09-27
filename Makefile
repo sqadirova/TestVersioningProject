8392d2afb4db71b34d0b6e10f848424bbbdd6114
Siddiga Gadirova <siddiga.gadirova@technofusion.az>
2022-09-27 15:18:52 +0400

    add versioning with gitcommit

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
 .PHONY: build
 
-#GIT_COMMIT := $(shell git rev-list -1 HEAD)
+GIT_COMMIT := $(shell git rev-list -1 HEAD)
+
+##go build -ldflags="-X 'main.Version=v2.1.0'"
 
 build:
-	go build -ldflags="-X 'main.Version=v2.1.0'"
+	go build -ldflags "-X main.gitCommit=$(GIT_COMMIT)" .
 run:
 	./testVersioning.exe
 
--- main.go
+++ main.go
@@ -2,46 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
-var Version = "development"
-
-//func printVersion() {
-//	log.Printf("Current build version: %s", Version)
-//}
-
-func main() {
-	//versionFlag := flag.Bool("v", false, "Print the current version and exit")
-	//flag.Parse()
-	//
-	//switch {
-	//case *versionFlag:
-	//	printVersion()
-	//	return
-	//}
-	// continue with other stuff
-	fmt.Println("Hello, world!!!")
-	//fmt.Println(versionFlag)
-	fmt.Println("Version:\t", Version)
-}
-
-//var gitCommit string
-//
-//func printVersion() {
-//	log.Printf("Current build version: %s", gitCommit)
-//}
+//var Version = "development"
 //
 //func main() {
-//	versionFlag := flag.Bool("v", false, "Print the current version and exit")
-//	flag.Parse()
-//
-//	switch {
-//	case *versionFlag:
-//		printVersion()
-//		return
-//	}
+//	//versionFlag := flag.Bool("v", false, "Print the current version and exit")
+//	//flag.Parse()
+//	//
+//	//switch {
+//	//case *versionFlag:
+//	//	printVersion()
+//	//	return
+//	//}
 //	// continue with other stuff
 //	fmt.Println("Hello, world!!!")
-//	fmt.Println(versionFlag)
-//	fmt.Println(gitCommit)
+//	//fmt.Println(versionFlag)
+//	fmt.Println("Version:\t", Version)
 //}
+
+var gitCommit string
+
+func printVersion() {
+	log.Printf("Current build version: %s", gitCommit)
+}
+
+func main() {
+	printVersion()
+
+	// continue with other stuff
+	fmt.Println("Hello, world!!!")
+}
